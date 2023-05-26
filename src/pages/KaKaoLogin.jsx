ef792274e53d026c992b1f6b7f50293fcced97d2
Hwang-uihyuk <weihyuk39@gmail.com>
2023-05-26 09:52:10 +0900

    feature:"kakao 로그인"

--- src/pages/KaKaoLogin.jsx
+++ src/pages/KaKaoLogin.jsx
@@ -1,7 +1,29 @@
+import axios from 'axios';
 import React from 'react'
+import { useLocation } from 'react-router-dom'
 
-export default function KaKaoLogin() {
+export default function KaoKaoLogin() {
+  const location = useLocation();
+  const KAKAO_CODE = location.search.split('=')[1];
+    console.log(KAKAO_CODE)
+
+  console.log("인증 코드 ", KAKAO_CODE)
+  const grant_type = "authorization_code";
+  const REST_API_KEY = process.env.REACT_REST_API_KEY;
+  const REDIRECT_URI = process.env.REACT_REDIRECT_URI
+
+    console.log(REST_API_KEY)
+  if(KAKAO_CODE){
+    axios.post(`https://kauth.kakao.com/oauth/token?grant_type=${grant_type}&client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&code=${KAKAO_CODE}`,
+    {},
+    {
+      headers : {
+        "Content-type" : "application/x-www-form-urlencoded; charset-utf-8",
+      },
+    }
+    ).then((res) => console.log("access token", res.data.access_token))
+  }
   return (
-    <div>KaKaoLogin</div>
+    <div>로그인 인증 코드과 토큰 </div>
   )
 }
