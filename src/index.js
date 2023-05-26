89f792f7fcac27fa8dafadfc3669b745f8f69bf9
Hwang-uihyuk <weihyuk39@gmail.com>
2023-05-26 09:40:15 +0900

    chore:"project setting"

--- src/index.js
+++ src/index.js
@@ -3,11 +3,50 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import KaKaoLogin from './pages/KaKaoLogin';
+import Home from './pages/Home';
+import StudyMap from './pages/StudyMap';
+import Board from './pages/Board';
+import Login from './pages/Login';
+
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element: <App/>,
+    children:[
+      {
+        index:true,
+        path:'/',
+        element: <Home/>
+      },
+      {
+        path:'/login',
+        element: <Login/>
+      },
+      {
+        path: '/oauth/callback/kakao',
+        element : <KaKaoLogin/>
+      },
+
+      {
+        path:'/map/study',
+        element: <StudyMap/>
+      },
+
+      {
+        path:'/board',
+        element: <Board/>
+      }
+    ]
+  }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router = {router} />
   </React.StrictMode>
 );
 
---
+++ src/pages/StudyMap.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function StudyMap() {
+  return (
+    <div>StudyMap</div>
+  )
+}
