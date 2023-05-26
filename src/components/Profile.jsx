02dc7ef4abe709f4512200ac6542373fb2e6b8ba
Hwang-uihyuk <weihyuk39@gmail.com>
2023-05-26 10:52:08 +0900

    feature:"profile component"

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -2,6 +2,47 @@ import React from 'react'
 
 export default function Profile() {
   return (
-    <div>Profile</div>
+      <div class="flex flex-col justify-center items-center h-[100vh]">
+            <div class="relative flex flex-col items-center rounded-[20px] w-[400px] mx-auto p-4 bg-white bg-clip-border shadow-3xl shadow-shadow-500 dark:!bg-navy-800 dark:text-white dark:!shadow-none">
+                <div class="relative flex h-32 w-full justify-center rounded-xl bg-cover" >
+                    <img src='https://horizon-tailwind-react-git-tailwind-components-horizon-ui.vercel.app/static/media/banner.ef572d78f29b0fee0a09.png' class="absolute flex h-32 w-full justify-center rounded-xl bg-cover"/> 
+                    <div class="absolute -bottom-12 flex h-[100px] w-[100px] items-center justify-center rounded-full border-[4px] border-white bg-pink-400 dark:!border-navy-700">
+                        <img class="h-full w-full rounded-full" src="profile1.png"/>
+                    </div>    
+                </div> 
+                
+                <div class="mt-16 flex flex-col items-center">
+                    <h4 class="text-xl font-bold text-navy-700 dark:text-white">
+                    Cha Hyeong Seok
+                    </h4>
+                    
+                    <p class="text-base font-normal text-gray-600">Backend Engineer</p>
+                    <div class="bg-slate-100 border rounded-full p-1.5">
+                      <button class="text-l font-bold">Follow</button>
+                    </div>
+                </div> 
+                <div class="mt-6 mb-3 flex gap-14 md:!gap-14">
+                    <div class="flex flex-col items-center justify-center">
+                    <p class="text-2xl font-bold text-navy-700 dark:text-white">320h</p>
+                    <p class="text-sm font-normal text-gray-600">Total</p>
+                    </div>
+                    <div class="flex flex-col items-center justify-center">
+                    <p class="text-2xl font-bold text-navy-700 dark:text-white">
+                        03:53:47
+                    </p>
+                    <p class="text-sm font-normal text-gray-600">Today</p>
+                    </div>
+                    <div class="flex flex-col items-center justify-center">
+                    <p class="text-2xl font-bold text-navy-700 dark:text-white">
+                        434
+                    </p>
+                    <p class="text-sm font-normal text-gray-600">Following</p>
+                    </div>
+                </div>
+                
+            </div>  
+            <p class="font-normal text-navy-700 mt-20 mx-auto w-max">Profile Card component <a href="https://horizon-ui.com?ref=tailwindcomponents.com" target="_blank" class="text-brand-500 font-bold">User Info</a></p>  
+            
+        </div>
   )
 }
