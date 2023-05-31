4514339c76ca32121f81e5a060db4a5e6b5793bf
Hwang-uihyuk <weihyuk39@gmail.com>
2023-05-31 16:53:06 +0900

    style:"navbar"

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,11 +4,26 @@ import Login from "../pages/Login";
 
 export default function Navbar() {
   return (
-    <header className="text-xl flex justify-between border border-black">
-      <Link to="/">Home</Link>
-      <Link to="/board">Board</Link>
-      <Link to="/mypage">Mypage</Link>
-      <Login />
+    <header className="text-xl flex justify-between border ">
+      {/* home */}
+      <div>
+      <Link to="/" className="flex">
+        <p className="text-6xl font-bold text-gray-700">S</p> <p className="pt-7">tudy</p>
+        <p className="text-6xl font-bold text-gray-700">W</p><p className="pt-7">ith</p>
+        <p className="text-6xl font-bold text-gray-700">M</p><p className="pt-7">e</p>
+      </Link>
+      </div>
+
+      <div className="flex">
+        <Link to="/board" className="pt-3.5">Board</Link>
+        <Link to="/mypage" className="pt-3.5 pl-5 pr-5">Mypage</Link>
+        <div className="pt-3.5">
+        <Login/>
+        </div>
+      </div>
     </header>
   );
 }
+
+
+
